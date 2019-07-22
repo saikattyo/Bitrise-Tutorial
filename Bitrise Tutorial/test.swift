414619776dc74f782a2fd87435f55855c57f58a0
斎藤克哉 <katsuya12321@gmail.com>
2019-07-22 16:01:45 +0900

    test

---
+++ Bitrise Tutorial/test.swift
@@ -0,0 +1,15 @@
+//
+//  test.swift
+//  Bitrise Tutorial
+//
+//  Created by 斎藤克哉 on 2019/07/21.
+//  Copyright © 2019 斎藤克哉. All rights reserved.
+//
+
+import UIKit
+
+class test: NSObject {
+    static func get(text: String) -> String {
+        return text
+    }
+}
--- Bitrise TutorialTests/Bitrise_TutorialTests.swift
+++ Bitrise TutorialTests/Bitrise_TutorialTests.swift
@@ -22,6 +22,7 @@ class Bitrise_TutorialTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual("test", test.get(text: "test"))
     }
 
     func testPerformanceExample() {
